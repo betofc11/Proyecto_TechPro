a1f7d1637dbedc943f80f6c08b8770e0aa89dab8
betofc11 <60046663+betofc11@users.noreply.github.com>
2021-04-05 19:30:39 -0600

    Javascript pagina principal

---
+++ Proyecto_TechPro/Acciones/app.js
@@ -0,0 +1,5 @@
+﻿function agregarCarrito(itemID) {
+    var item = $(itemID).attr("itemid");
+    sessionStorage.setItem("ejemplo", item);
+    alert("El id del item es: "+sessionStorage.getItem("ejemplo"));
+}
