22d55162af82a5f2089cc7de81256ff21055220f
betofc11 <60046663+betofc11@users.noreply.github.com>
2021-04-24 16:28:04 -0600

    Registro

--- Proyecto_TechPro/Controllers/LoginController.cs
+++ Proyecto_TechPro/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto_TechPro.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,6 +22,42 @@ namespace Proyecto_TechPro.Controllers
         public ActionResult Registrarse()
         {
             return View();
+        }
+
+        public ActionResult RegistrarUsuario(Usuario user)
+        {
+            using (var contexto = new ProyectoPrograEntities())
+            {
+                var resultado = (from x in contexto.usuario
+                                 where x.email == user.email
+                                 select x).FirstOrDefault();
+                if (resultado == null && 
+                    user.idUsuario > 0 && 
+                    user.email != null && 
+                    user.nombre != null &&
+                    user.primerApellido != null &&
+                    user.segundoApellido != null &&
+                    user.Pass != null &&
+                    user.Pass2 != null &&
+                    user.telefono > 0)
+                {
+                    if (user.Pass == user.Pass2)
+                    {
+                        usuario us = new usuario();
+                        us.idUsuario = user.idUsuario;
+                        us.email = user.email;
+                        us.nombre = user.nombre;
+                        us.primerApellido = user.primerApellido;
+                        us.segundoApellido = user.segundoApellido;
+                        us.telefono = user.telefono;
+                        us.Pass = user.Pass;
+                        contexto.SaveChanges();
+
+                        return View("Login");
+                    }
+                }
+                return View("Registrarse");
+            }
         }
 
         public ActionResult RecuperarContrasena()
