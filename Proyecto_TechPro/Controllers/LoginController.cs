a4b9e21cf69fb63707f94359b205679b7fbc421e
JOSE CABRERA <jcabrera58035@ufide.ac.cr>
2021-04-24 16:59:39 -0600

    comente registrar ususario osino no corre

--- Proyecto_TechPro/Controllers/LoginController.cs
+++ Proyecto_TechPro/Controllers/LoginController.cs
@@ -24,42 +24,42 @@ namespace Proyecto_TechPro.Controllers
             return View();
         }
 
-        public ActionResult RegistrarUsuario(Usuario user)
-        {
-            using (var contexto = new ProyectoPrograEntities())
-            {
-                var resultado = (from x in contexto.usuario
-                                 where x.email == user.email
-                                 select x).FirstOrDefault();
-                if (resultado == null && 
-                    user.idUsuario > 0 && 
-                    user.email != null && 
-                    user.nombre != null &&
-                    user.primerApellido != null &&
-                    user.segundoApellido != null &&
-                    user.Pass != null &&
-                    user.Pass2 != null &&
-                    user.telefono > 0)
-                {
-                    if (user.Pass == user.Pass2)
-                    {
-                        usuario us = new usuario();
-                        us.idUsuario = user.idUsuario;
-                        us.email = user.email;
-                        us.nombre = user.nombre;
-                        us.primerApellido = user.primerApellido;
-                        us.segundoApellido = user.segundoApellido;
-                        us.telefono = user.telefono;
-                        us.Pass = user.Pass;
-                        contexto.usuario.Add(us);
-                        contexto.SaveChanges();
+        //public ActionResult RegistrarUsuario(Usuario user)
+        //{
+        //    using (var contexto = new ProyectoPrograEntities())
+        //    {
+        //        var resultado = (from x in contexto.usuario
+        //                         where x.email == user.email
+        //                         select x).FirstOrDefault();
+        //        if (resultado == null && 
+        //            user.idUsuario > 0 && 
+        //            user.email != null && 
+        //            user.nombre != null &&
+        //            user.primerApellido != null &&
+        //            user.segundoApellido != null &&
+        //            user.Pass != null &&
+        //            user.Pass2 != null &&
+        //            user.telefono > 0)
+        //        {
+        //            if (user.Pass == user.Pass2)
+        //            {
+        //                usuario us = new usuario();
+        //                us.idUsuario = user.idUsuario;
+        //                us.email = user.email;
+        //                us.nombre = user.nombre;
+        //                us.primerApellido = user.primerApellido;
+        //                us.segundoApellido = user.segundoApellido;
+        //                us.telefono = user.telefono;
+        //                us.Pass = user.Pass;
+        //                contexto.usuario.Add(us);
+        //                contexto.SaveChanges();
 
-                        return View("Login");
-                    }
-                }
-                return View("Registrarse");
-            }
-        }
+        //                return View("Login");
+        //            }
+        //        }
+        //        return View("Registrarse");
+        //    }
+        //}
 
         public ActionResult RecuperarContrasena()
         {
