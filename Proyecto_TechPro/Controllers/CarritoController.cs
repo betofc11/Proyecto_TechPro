2f003e2c5a864419f2d146553b99c4fc6cb902af
JOSE CABRERA <jcabrera58035@ufide.ac.cr>
2021-04-24 23:53:17 -0600

    Carrito Completo

--- Proyecto_TechPro/Controllers/CarritoController.cs
+++ Proyecto_TechPro/Controllers/CarritoController.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+//using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Proyecto_TechPro.Entidades;
+using System.Web.Mvc.Async;
 
 namespace Proyecto_TechPro.Controllers
 {
     public class CarritoController : Controller
     {
-        // GET: Carrito
-   
 
 
         public ActionResult CarritoView()
         {
-            var nuevos = "";
-            //int idPs = int.Parse(idp);
+
+            //int idPs = int.Parse(idp);k
+            //int idPs = int.Parse(idp);k
             using (var contexto = new ProyectoPrograEntities())
             {
 
@@ -24,50 +26,133 @@ namespace Proyecto_TechPro.Controllers
                 List<Producto> prods = new List<Producto>();
                 prods = (List<Producto>)Session["ProductosCarrito"];
 
-
-
-                if (Session["ProductosCarrito"] == null)
+                var Suma = 0;
+                if (Session["ProductosCarrito"] != null)
                 {
-                    ViewBag.Message = "NO HAY NADA";
-
-                }
-                else
-                {
-                  
-                    List<Producto> prodNuevos = new List<Producto>();
                     foreach (var items in prods)
                     {
-
-                       
-                        
-                        prodNuevos.Add(new Producto
-                        {
-                            idProducto = items.idProducto,
-                            nombreProducto = items.nombreProducto,
-                            precio = items.precio,
-                            idCategoria = items.idCategoria,
-                            imagen = items.imagen,
-                            descripcion = items.descripcion
-                        });
-
-
-
-                        var resultado = (from x in contexto.Productos
-                                         where x.idProducto == items.idProducto
-                                 select x).ToList();
-
-                        
-                         View(resultado);
+                        Suma = (int)(Suma + items.precio);
                     }
-                    
 
                 }
-              
-                return View();
+                ViewBag.Total = Suma;
+                return View(prods);
             }
-        
+
+        }
+        public ActionResult EliminaCarrito(int Producto)
+        {
+
+
+            using (var contexto = new ProyectoPrograEntities())
+            {
+
+
+                List<Producto> prods = new List<Producto>();
+
+                prods = (List<Producto>)Session["ProductosCarrito"];
+                var eliminado = false;
+                    for(int i=0;i<=prods.Count-1;i++)
+                    {
+                    //while (eliminado != true) 
+                    //{
+                        var p = prods[i];
+                        if (p.idProducto == Producto)
+                        {
+                            var x = prods.Remove(p);
+                            Session["ProductosCarrito"] = prods;
+                            Session["CantidadCarrito"] = prods.Count;
+                            eliminado = true;
+                            i = prods.Count;
+                        }
+                        else
+                        {
+                            i++;
+                        }
+                        
+                    //}
+                       
+                    }
+                
+              
+                return View (prods);
+              
+            }
         }
 
+        // GET: Carrito
+
+
+
+        //public ActionResult CarritoView()
+        //{
+        //    var nuevos = "";
+        //    //int idPs = int.Parse(idp);
+        //    using (var contexto = new ProyectoPrograEntities())
+        //    {
+
+
+        //        List<Producto> prods = new List<Producto>();
+        //        prods = (List<Producto>)Session["ProductosCarrito"];
+
+
+
+        //        if (Session["ProductosCarrito"] == null)
+        //        {
+        //            ViewBag.Message = "NO HAY NADA";
+
+        //        }
+        //        else
+        //        {
+
+        //            List<Producto> prodNuevos = new List<Producto>();
+        //            foreach (var items in prods)
+        //            {
+
+
+
+        //                prodNuevos.Add(new Producto
+        //                {
+        //                    idProducto = items.idProducto,
+        //                    nombreProducto = items.nombreProducto,
+        //                    precio = items.precio,
+        //                    idCategoria = items.idCategoria,
+        //                    imagen = items.imagen,
+        //                    descripcion = items.descripcion
+        //                });
+
+
+
+        //                var resultado = (from x in contexto.Productos
+        //                                 where x.idProducto == items.idProducto
+        //                         select x).ToList();
+
+
+        //                 View(resultado);
+        //            }
+
+
+        //        }
+
+        //        return View();
+        //    }
+
+        //}
+        //public Task<ActionResult> VistaProductos(string id)
+        //{
+        //    var idP = Int64.Parse(id);
+        //    string Produc = " ";
+        //    using (var contexto = new ProyectoPrograEntities())
+        //    {
+        //        var Productos = (from x in contexto.Productos
+        //                         where x.idProducto == idP
+        //                         select x).FirstOrDefault();
+
+        //        Produc = Productos.ToString();
+
+        //    }
+        //    return (Productos);
+        //}
         public ActionResult FinalizarPedido()
         {
             ViewBag.Message = "Your application description page.";
@@ -76,7 +161,7 @@ namespace Proyecto_TechPro.Controllers
         }
     }
 
-   
+
 
 
 }
